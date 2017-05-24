d8f9d9307ed18aeb7b55b94b8bc9180169cc29cb
Mattias Sundling <masu1402@student.miun.se>
2017-05-24 11:57:00 +0200

    Update for a final commit. Changed incorrect REST url, removed unnecessary packages

--- reader.js
+++ reader.js
@@ -1,3 +1,16 @@
+/**
+ * reader.js
+ * Author: Mattias Sundling, masu1402@student.miun.se
+ * Latest update: 2017-05-24
+ * 
+ * This file servers as the main server of a rfid reader.
+ * It asks for setup variables in command line and loops reading from
+ * rfid module rc522 with library mfrc522-rpi. If it catches a valid
+ * serial number, it caches it for 2 seconds and sends a POST HTTP-request
+ * to the RFID REST API with race, user and node identifier variables.
+ * This was created as a proof of concept during the final independent project
+ * in Computer Science.
+ */
 // set up =============================================== set up 
 var express = require("express");
 var app = express();
@@ -5,7 +18,6 @@ var read = require("read");
 var request = require("request");
 var mfrc522 = new (require("mfrc522-rpi"));
 const cache = new(require("node-cache"));
-var readline = require('readline');
 
 // listen (start app with node reader.js) =============== listener 
 app.listen(3000, function() {
@@ -35,7 +47,7 @@ app.listen(3000, function() {
 		
 		// builds PATCH-url by using cached variables set at start
 		try {
-		options.url = "https://bor-rest-masu1402.c9users.io/api/race/" + cache.get("prompt").rid;
+		options.url = "https://bor-rest-masu1402.c9users.io/api/races/" + cache.get("prompt").rid;
 		options.url += "/competitors/" + cache.get("reading").uid;
 		options.url += "/readings/" + cache.get("prompt").nid; 
 		options.body = cache.get("reading");
@@ -43,7 +55,7 @@ app.listen(3000, function() {
 			console.error(error)
 		}
 
-		// send PATCH-request to REST API with Digest authentication
+		// send POST-request to REST API with Digest authentication
 		request(options, function(error, response, body){
 			if (error) console.error(error);
 			console.log(options);
@@ -54,7 +66,7 @@ app.listen(3000, function() {
 
 var options = {
 	"url": "https://bor-rest-masu1402.c9users.io/api/",
-	"method": "PATCH",
+	"method": "POST",
 	"port": 8080,
 	"auth": {
 		"user": "",
